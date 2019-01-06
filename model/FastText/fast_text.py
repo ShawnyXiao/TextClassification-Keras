0ee6cd2f66f122822a281b9beb97190a27c2c6af
ShawnyXiao <772077730@qq.com>
2019-01-06 16:05:40 +0800

    Add FastText

---
+++ model/FastText/fast_text.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+from keras import Input, Model
+from keras.layers import Embedding, GlobalAveragePooling1D, Dense
+
+
+class FastText(object):
+    def __init__(self, maxlen, max_features, embedding_dims,
+                 class_num=1,
+                 last_activation='sigmoid'):
+        self.maxlen = maxlen
+        self.max_features = max_features
+        self.embedding_dims = embedding_dims
+        self.class_num = class_num
+        self.last_activation = last_activation
+
+    def get_model(self):
+        input = Input((self.maxlen,))
+
+        embedding = Embedding(self.max_features, self.embedding_dims, input_length=self.maxlen)(input)
+        x = GlobalAveragePooling1D()(embedding)
+
+        output = Dense(self.class_num, activation=self.last_activation)(x)
+        model = Model(inputs=input, outputs=output)
+        return model
