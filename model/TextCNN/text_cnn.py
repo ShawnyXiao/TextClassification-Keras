39375b10aa05325b438c35d897c6e9cafe57ad58
ShawnyXiao <772077730@qq.com>
2019-01-06 16:02:58 +0800

    Add TextCNN

---
+++ model/TextCNN/text_cnn.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+
+from keras import Input, Model
+from keras.layers import Embedding, Dense, Conv1D, GlobalMaxPooling1D, Concatenate, Dropout
+
+
+class TextCNN(object):
+    def __init__(self, maxlen, max_features, embedding_dims,
+                 class_num=1,
+                 last_activation='sigmoid'):
+        self.maxlen = maxlen
+        self.max_features = max_features
+        self.embedding_dims = embedding_dims
+        self.class_num = class_num
+        self.last_activation = last_activation
+
+    def get_model(self):
+        input = Input((self.maxlen,))
+
+        # Embedding part can try multichannel as same as origin paper
+        embedding = Embedding(self.max_features, self.embedding_dims, input_length=self.maxlen)(input)
+        convs = []
+        for kernel_size in [3, 4, 5]:
+            c = Conv1D(128, kernel_size, activation='relu')(embedding)
+            c = GlobalMaxPooling1D()(c)
+            convs.append(c)
+        x = Concatenate()(convs)
+        x = Dropout(0.5)(x)
+
+        output = Dense(self.class_num, activation=self.last_activation)(x)
+        model = Model(inputs=input, outputs=output)
+        return model
